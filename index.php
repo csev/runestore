58361e8a2a491830751de95ea81543f214c7dbaf
Charles Severance <csev@umich.edu>
2017-06-22 08:18:51 -0400

    Tweaks

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
-<h1>Hello</h1>
+<h1>Welcome to the RuneStore</h1>
 <p>
+<ul>
+<li>
 Go to 
 <a href="lessons">lessons</a>
+</li>
+<li>
+Here is your 
+<a href="tsugi/lti/store/canvas-config.xml" target="_new">Canvas Installation Link</a></li>
+<li>
+Here is your 
+<a href="tsugi/lti/store/" target="_new">Sakai Installation Link</a>
+(It won't work in the new window since it is expecting LTI - paste it into Sakai).
+</ul>
 </p>
